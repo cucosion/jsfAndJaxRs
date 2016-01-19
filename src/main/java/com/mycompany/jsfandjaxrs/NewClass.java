051bfeeb428c4b6919f82ed0dee7ab2dc37caa67
cucos <cucos@CUCOS3.emea.cpqcorp.net>
2016-01-19 22:09:19 +0200

    new commit

--- src/main/java/com/mycompany/jsfandjaxrs/NewClass.java
+++ src/main/java/com/mycompany/jsfandjaxrs/NewClass.java
@@ -5,10 +5,43 @@
  */
 package com.mycompany.jsfandjaxrs;
 
+import java.util.Objects;
+
 /**
  *
  * @author cucos
  */
 public class NewClass {
+    String somechanges;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.somechanges);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final NewClass other = (NewClass) obj;
+        if (!Objects.equals(this.somechanges, other.somechanges)) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getSomechanges() {
+        return somechanges;
+    }
+
+    public void setSomechanges(String somechanges) {
+        this.somechanges = somechanges;
+    }
     
 }
