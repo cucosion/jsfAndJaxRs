13d10ef2808f95c2924f1822f4e840fb3d3cb110
cucos <cucos@CUCOS3.emea.cpqcorp.net>
2016-01-19 22:32:27 +0200

    new commit

---
+++ src/main/java/com/mycompany/jsfandjaxrs/NewClass1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jsfandjaxrs;
+
+/**
+ *
+ * @author cucos
+ */
+public class NewClass1 {
+ String s;   
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+ 
+ 
+ 
+}
